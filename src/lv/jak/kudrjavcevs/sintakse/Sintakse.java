971fd9a781c17f2c31bc40e9ba58ce2f5ab5547f
lietotajs <lietotajs@121-09>
2019-12-20 10:57:09 +0200

    Aprakstīta klase SINTAKSE

---
+++ src/lv/jak/kudrjavcevs/sintakse/Sintakse.java
@@ -0,0 +1,24 @@
+// Komentārs - programmas koda daļa, kas palīdz programmas izstrādātājiem labāk izprast labāk izprast koda darbibas principus.
+// Programmas darbības laikā komentāri netiek ņemti vērā.
+
+// - Vienas rindas komentārs
+
+/* Vairāku
+ * rindu
+ * komentārs
+ */
+ 
+package lv.jak.kudrjavcevs.sintakse;  //pakotne - klases atrašanās vieta projektā
+
+// import komanda - komandarinda, kas nodrošina citas klases vai pakotnes piesaisti šai klasei.
+// Tādā veidā tiek nodrošinātas dažādu funkciju izmantošanas iespējas šajā klasē, piemēram,
+// datu ievade no tastatūras.
+
+public class Sintakse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
